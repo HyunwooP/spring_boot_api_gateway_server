d7a7983cb3962036667b5ea68050551d5f818a78
harry <awakelife93@gmail.com>
2021-07-06 19:46:23 +0900

    클라이언트만 허용한다.

---
+++ src/main/java/proj/gateway/apigateway/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package proj.gateway.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  // todo = 3000, 3001, 3002 각 도메인들 한곳으로 따놓기
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+  }
+}
