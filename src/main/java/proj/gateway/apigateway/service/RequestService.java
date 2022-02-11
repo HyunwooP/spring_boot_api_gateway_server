471c4812c701ebd89bf42ef70931c243d40d604c
harry <awakelife93@gmail.com>
2022-02-11 23:45:17 +0900

    service 서킷브레이커 어노테이션 및 fallback 추가

--- src/main/java/proj/gateway/apigateway/service/RequestService.java
+++ src/main/java/proj/gateway/apigateway/service/RequestService.java
@@ -7,28 +7,46 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import proj.gateway.apigateway.common.service.CommonService;
 
 @Service("requestService")
 public class RequestService extends CommonService {
 
+  @CircuitBreaker(name = "get", fallbackMethod = "queryFallBack")
   public HashMap<String, Object> get(HttpServletRequest req) throws Exception {
     return queryRequest(req);
   }
 
+  @CircuitBreaker(name = "delete", fallbackMethod = "queryFallBack")
   public HashMap<String, Object> delete(HttpServletRequest req) throws Exception {
     return queryRequest(req);
   }
 
+  @CircuitBreaker(name = "post", fallbackMethod = "bodyFallBack")
   public HashMap<String, Object> post(HttpServletRequest req, Map<String, Object> body) throws Exception {
     return bodyRequest(req, body);
   }
 
+  @CircuitBreaker(name = "put", fallbackMethod = "bodyFallBack")
   public HashMap<String, Object> put(HttpServletRequest req, Map<String, Object> body) throws Exception {
     return bodyRequest(req, body);
   }
 
+  @CircuitBreaker(name = "patch", fallbackMethod = "bodyFallBack")
   public HashMap<String, Object> patch(HttpServletRequest req, Map<String, Object> body) throws Exception {
     return bodyRequest(req, body);
+  }
+
+  public HashMap<String, Object> queryFallBack(HttpServletRequest req, Throwable t) throws Exception {
+    System.out.println("=========== queryFallBack Call!!! ===========");
+    throw new Exception(t.getMessage());
+  }
+
+  public HashMap<String, Object> bodyFallBack(HttpServletRequest req, Map<String, Object> body,
+      CallNotPermittedException t) throws Exception {
+    System.out.println("=========== bodyFallBack Call!!! ===========");
+    throw new Exception(t.getMessage());
   }
 }
