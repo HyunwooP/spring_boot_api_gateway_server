035f6cfbdffe5d95a4d19bb08c338eab5ec811ba
harry <awakelife93@gmail.com>
2022-02-12 21:21:05 +0900

    fallback 메소드명 수정 및 exception 인자 타입 변경

--- src/main/java/proj/gateway/apigateway/service/RequestService.java
+++ src/main/java/proj/gateway/apigateway/service/RequestService.java
@@ -7,46 +7,43 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
 
-import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import proj.gateway.apigateway.common.service.CommonService;
 
 @Service("requestService")
 public class RequestService extends CommonService {
 
-  @CircuitBreaker(name = "get", fallbackMethod = "queryFallBack")
+  @CircuitBreaker(name = "get", fallbackMethod = "queryRequestFallBack")
   public HashMap<String, Object> get(HttpServletRequest req) throws Exception {
     return queryRequest(req);
   }
 
-  @CircuitBreaker(name = "delete", fallbackMethod = "queryFallBack")
+  @CircuitBreaker(name = "delete", fallbackMethod = "queryRequestFallBack")
   public HashMap<String, Object> delete(HttpServletRequest req) throws Exception {
     return queryRequest(req);
   }
 
-  @CircuitBreaker(name = "post", fallbackMethod = "bodyFallBack")
+  @CircuitBreaker(name = "post", fallbackMethod = "bodyRequestFallBack")
   public HashMap<String, Object> post(HttpServletRequest req, Map<String, Object> body) throws Exception {
     return bodyRequest(req, body);
   }
 
-  @CircuitBreaker(name = "put", fallbackMethod = "bodyFallBack")
+  @CircuitBreaker(name = "put", fallbackMethod = "bodyRequestFallBack")
   public HashMap<String, Object> put(HttpServletRequest req, Map<String, Object> body) throws Exception {
     return bodyRequest(req, body);
   }
 
-  @CircuitBreaker(name = "patch", fallbackMethod = "bodyFallBack")
+  @CircuitBreaker(name = "patch", fallbackMethod = "bodyRequestFallBack")
   public HashMap<String, Object> patch(HttpServletRequest req, Map<String, Object> body) throws Exception {
     return bodyRequest(req, body);
   }
 
-  public HashMap<String, Object> queryFallBack(HttpServletRequest req, Throwable t) throws Exception {
-    System.out.println("=========== queryFallBack Call!!! ===========");
+  public HashMap<String, Object> queryRequestFallBack(HttpServletRequest req, Throwable t) throws Exception {
     throw new Exception(t.getMessage());
   }
 
-  public HashMap<String, Object> bodyFallBack(HttpServletRequest req, Map<String, Object> body,
-      CallNotPermittedException t) throws Exception {
-    System.out.println("=========== bodyFallBack Call!!! ===========");
+  public HashMap<String, Object> bodyRequestFallBack(HttpServletRequest req, Map<String, Object> body, Throwable t)
+      throws Exception {
     throw new Exception(t.getMessage());
   }
 }
