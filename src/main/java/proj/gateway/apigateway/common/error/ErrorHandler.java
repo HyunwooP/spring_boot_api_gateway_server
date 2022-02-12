d32ab35234312b85e7f1418ff435bab39c240766
harry <awakelife93@gmail.com>
2022-02-12 21:21:53 +0900

    공통 에러 처리 로직 수정 및 CircuitBreaker 내용 반영

--- src/main/java/proj/gateway/apigateway/common/error/ErrorHandler.java
+++ src/main/java/proj/gateway/apigateway/common/error/ErrorHandler.java
@@ -35,6 +35,32 @@ public class ErrorHandler {
     errorMap.put(500, "INTERNAL_SERVER_ERROR");
   }
 
+  private int getErrorCode(String[] messageArray) {
+
+    if (messageArray[0].equals("CircuitBreaker")) {
+      return 429;
+    }
+
+    if (messageArray.length < 2) {
+      // Node Server에서 404를 리턴할 경우, Spring에서 FindNotFoundException으로 Catch하기 때문에 따로
+      // 404처리
+      return 404;
+    } else {
+      return Integer.parseInt(messageArray[5]);
+    }
+  }
+
+  // 해당 메소드를 통해서 원초적인 에러 메세지를 얻을지 상태 에러 메세지를 얻을지 결정
+  private boolean checkGetErrorMessage(int code) {
+    if (code == 429)
+      return false;
+    return true;
+  }
+
+  private String getErrorMessage(int code) {
+    return errorMap.get(code);
+  }
+
   /**
    * errorHandler
    *
@@ -45,23 +71,17 @@ public class ErrorHandler {
   private ResponseEntity<Object> errorHandler(Exception e) {
     // ex: Server returned HTTP response code: 403 for URL:
     // http://localhost:3001/findUserProfile
-    String[] error = e.getMessage().split(" ");
-    int code;
+    String errorMessage = e.getMessage();
+    String[] messageArray = errorMessage.split(" ");
+    int code = getErrorCode(messageArray);
+    String message = checkGetErrorMessage(code) ? getErrorMessage(code) : errorMessage;
 
     System.out.println("===========");
-    System.out.println(e.getMessage());
+    System.out.println(errorMessage);
     System.out.println("===========");
-
-    // Node Server에서 404를 리턴할 경우, Spring에서 FindNotFoundException으로 제어한다.
-    // todo: CircuitBreaker를 붙이면서 더 이상 조건에 의한 에러 구분을 하지 못함. 아래 리팩토링 필요
-    if (error.length < 2) {
-      code = 404;
-    } else {
-      code = Integer.parseInt(error[5]);
-    }
 
     returnMap.put("status", code);
-    returnMap.put("message", errorMap.get(code));
+    returnMap.put("message", message);
 
     return ResponseEntity.status(code).body(returnMap);
   }
