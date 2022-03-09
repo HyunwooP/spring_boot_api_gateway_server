22b43cb45c8621aaa28705c4004c7f7d588de03c
harry <awakelife93@gmail.com>
2022-03-09 16:28:58 +0900

    주석 삭제 및 설계 변경으로 인한 리팩토링

--- src/main/java/proj/gateway/apigateway/controller/RequestController.java
+++ src/main/java/proj/gateway/apigateway/controller/RequestController.java
@@ -8,31 +8,140 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 
-import proj.gateway.apigateway.service.RequestService;
+import proj.gateway.apigateway.service.AuthService;
+import proj.gateway.apigateway.service.ComponentService;
+import proj.gateway.apigateway.service.ContentsService;
+import proj.gateway.apigateway.service.LayoutService;
+import proj.gateway.apigateway.service.StyleService;
+import proj.gateway.apigateway.service.ThemeService;
+import proj.gateway.apigateway.service.UserService;
 
-@Controller("requestController")
+@Controller("RequestController")
 public class RequestController {
 
-  @Resource(name = "requestService")
-  RequestService requestService;
+  @Resource(name = "UserService")
+  UserService userService;
+
+  @Resource(name = "AuthService")
+  AuthService authService;
+
+  @Resource(name = "ContentsService")
+  ContentsService contentsService;
+
+  @Resource(name = "ComponentService")
+  ComponentService componentService;
+
+  @Resource(name = "LayoutService")
+  LayoutService layoutService;
+
+  @Resource(name = "StyleService")
+  StyleService styleService;
+
+  @Resource(name = "ThemeService")
+  ThemeService themeService;
 
   public HashMap<String, Object> get(HttpServletRequest req) throws Exception {
-    return requestService.get(req);
-  }
+    String path = req.getRequestURI();
 
-  public HashMap<String, Object> delete(HttpServletRequest req) throws Exception {
-    return requestService.delete(req);
+    switch (path) {
+      case "/findUser":
+        return userService.findUser(req);
+      case "/findUserCount":
+        return userService.findUserCount(req);
+      case "/findUserProfile":
+        return userService.findUserProfile(req);
+      case "/findContentsCount":
+        return contentsService.findContentsCount(req);
+      case "/findContents":
+        return contentsService.findContents(req);
+      case "/findComponentCount":
+        return componentService.findComponentCount(req);
+      case "/findComponent":
+        return componentService.findComponent(req);
+      case "/findLayoutCount":
+        return layoutService.findLayoutCount(req);
+      case "/findLayout":
+        return layoutService.findLayout(req);
+      case "/findStyleCount":
+        return styleService.findStyleCount(req);
+      case "/findStyle":
+        return styleService.findStyle(req);
+      case "/findThemeCount":
+        return themeService.findThemeCount(req);
+      case "/findThemeItem":
+        return themeService.findThemeItem(req);
+      case "/findTheme":
+        return themeService.findTheme(req);
+
+      default:
+        throw new Error("That API doesn't exist.");
+    }
   }
 
   public HashMap<String, Object> post(HttpServletRequest req, Map<String, Object> body) throws Exception {
-    return requestService.post(req, body);
+    String path = req.getRequestURI();
+
+    switch (path) {
+      case "/signInUser":
+        return authService.signInUser(req, body);
+      case "/signInAdmin":
+        return authService.signInAdmin(req, body);
+      case "/signUp":
+        return authService.signUp(req, body);
+      case "/signOut":
+        return authService.signOut(req, body);
+      case "/createContents":
+        return contentsService.createContents(req, body);
+
+      default:
+        throw new Error("That API doesn't exist.");
+    }
+  }
+
+  public HashMap<String, Object> delete(HttpServletRequest req) throws Exception {
+    String path = req.getRequestURI();
+
+    switch (path) {
+      case "/removeUser":
+        return userService.removeUser(req);
+      case "/tokenRemoveUser":
+        return userService.tokenRemoveUser(req);
+      case "/removeContents":
+        return contentsService.removeContents(req);
+      case "/removeComponent":
+        return componentService.removeComponent(req);
+      case "/removeLayout":
+        return layoutService.removeLayout(req);
+      case "/removeStyle":
+        return styleService.removeStyle(req);
+      case "/removeTheme":
+        return themeService.removeTheme(req);
+
+      default:
+        throw new Error("That API doesn't exist.");
+    }
   }
 
   public HashMap<String, Object> put(HttpServletRequest req, Map<String, Object> body) throws Exception {
-    return requestService.put(req, body);
+    String path = req.getRequestURI();
+
+    switch (path) {
+      default:
+        throw new Error("That API doesn't exist.");
+    }
   }
 
   public HashMap<String, Object> patch(HttpServletRequest req, Map<String, Object> body) throws Exception {
-    return requestService.patch(req, body);
+    String path = req.getRequestURI();
+
+    switch (path) {
+      case "/updateUser":
+        return userService.updateUser(req, body);
+      case "/updateContents":
+        return contentsService.updateContents(req, body);
+
+      default:
+        throw new Error("That API doesn't exist.");
+    }
   }
 }
