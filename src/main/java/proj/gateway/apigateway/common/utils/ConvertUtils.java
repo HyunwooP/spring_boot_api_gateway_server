43891ebe101b060b4bb8ff415129431a5bd6c6c9
hw.park <hw.park@fastfive.co.kr>
2022-03-11 19:55:04 +0900

    좀 더 명확한 에러로 수정

--- src/main/java/proj/gateway/apigateway/common/utils/ConvertUtils.java
+++ src/main/java/proj/gateway/apigateway/common/utils/ConvertUtils.java
@@ -2,6 +2,7 @@ package proj.gateway.apigateway.common.utils;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.stereotype.Component;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ConvertUtils {
 
-  public static String objectToJsonString(Map<String, Object> params) throws Exception {
+  public static String objectToJsonString(Map<String, Object> params) throws JsonProcessingException {
     ObjectMapper mapper = new ObjectMapper();
     String jsonParams = mapper.writeValueAsString(params);
 
