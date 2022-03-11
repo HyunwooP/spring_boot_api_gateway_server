ee87ba61fd3b854c45824e6a2f55618c08e66c84
hw.park <hw.park@fastfive.co.kr>
2022-03-11 19:52:12 +0900

    interceptor 추가 및 그에 따른 파일 이름 변경

--- src/main/java/proj/gateway/apigateway/config/ProtocolConfig.java
+++ src/main/java/proj/gateway/apigateway/config/ProtocolConfig.java
@@ -2,10 +2,13 @@ package proj.gateway.apigateway.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import proj.gateway.apigateway.common.component.Interceptor;
+
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class ProtocolConfig implements WebMvcConfigurer {
 
   // todo = 3000, 3001, 3002 각 도메인들 한곳으로 따놓기
   @Override
@@ -13,4 +16,9 @@ public class CorsConfig implements WebMvcConfigurer {
     registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:3003").allowedMethods("GET",
         "PUT", "DELETE", "POST", "PATCH");
   }
+
+  @Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new Interceptor()).addPathPatterns("/**");
+	}
 }
