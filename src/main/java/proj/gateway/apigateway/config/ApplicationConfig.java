dcb5e67fb91e2e1428800d29cfd5afacc2e2dfe7
hw.park <hw.park@fastfive.co.kr>
2022-03-11 19:52:43 +0900

    not found error를 제어하기 위한 config 추가

---
+++ src/main/java/proj/gateway/apigateway/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package proj.gateway.apigateway.config;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class ApplicationConfig {
+  
+  @Resource
+  void configureDispatcherServlet(DispatcherServlet dispatcherServlet) {
+    dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+  }
+}
