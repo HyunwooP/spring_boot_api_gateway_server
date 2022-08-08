2e7870bc1ca997dcbe4e814c50be4270c81b3842
harry <awakelife93@gmail.com>
2022-08-08 21:02:04 +0900

    prettier write

--- src/main/java/com/awakelife93/apigateway/service/auth/AuthService.java
+++ src/main/java/com/awakelife93/apigateway/service/auth/AuthService.java
@@ -8,10 +8,10 @@ import com.awakelife93.apigateway.common.error.exceptions.APIResponseException;
 
 public interface AuthService {
 
-        public Map<String, Object> signIn(HttpServletRequest request, Map<String, Object> body)
-                        throws APIResponseException;
+  public Map<String, Object> signIn(HttpServletRequest request, Map<String, Object> body)
+      throws APIResponseException;
 
-        public Map<String, Object> signOut(HttpServletRequest request, Map<String, Object> body)
-                        throws APIResponseException;
+  public Map<String, Object> signOut(HttpServletRequest request, Map<String, Object> body)
+      throws APIResponseException;
 
 }
